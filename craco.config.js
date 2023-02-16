4851827dc41acacc6674c7a2af3918130b5a74f6
ruijiefan <ruijiefan94@outlook.com>
2023-02-17 02:35:16 +0800

    change webpack

--- craco.config.js
+++ craco.config.js
@@ -22,19 +22,6 @@ module.exports = {
       "@": resolve("src"),
       "components": resolve("src/components"),
       // '@mui/styled-engine': '@mui/styled-engine-sc'
-    },
-    configure: (config) => {
-      config.output.library = `airbnb`;
-      config.output.libraryTarget = 'umd';
-      config.output.publicPath = '//localhost:3010/'
-      return config;
-    },
-    devServer: (_) => {
-      const config = _;
-      config.headers = {
-        'Access-Control-Allow-Origin': '*',
-      };
-      return config;
-    },
+    }
   }
 }
